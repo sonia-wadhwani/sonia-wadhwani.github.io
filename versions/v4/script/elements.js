4b2bd40ae3dbaf071b253cb06e371d7262e6a5ac
sonia.d.wadhwani@gmail.com <sonia.d.wadhwani@gmail.com>
2024-11-13 21:06:59 +0530

    removed versions outside

--- versions/v4/script/elements.js
+++
@@ -1,9 +0,0 @@
-const imageElement = document.getElementById('image');
-const radioButtons = document.querySelectorAll('input[name="image"]');
-
-radioButtons.forEach(radio => {
-  radio.addEventListener('change', () => {
-    const selectedImage = radio.value;
-    imageElement.src = `images/${selectedImage}.png`;
-  });
-});
